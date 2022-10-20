a03d24c6dc10417315770acce8fab65dcee33c30
bbakercello <bbakercello@gmail.com>
2022-10-20 16:51:56 -0400

    readying for deployment

--- components/About.js
+++ components/About.js
@@ -13,7 +13,7 @@ const About = () => {
             <img src='https://i.imgur.com/9diMR92.jpg' alt='Profile' className='w-60 rounded-lg'></img>
             </div>
             <div className='w-80'>
-                <h6 className='pt-6 font-bold'>I'm Ben</h6>
+                <h6 className='pt-6 font-bold'>Ben Baker</h6>
                 <p className='pt-4 text-xs'> I'm a full-stack engineer who loves developing core infrastructures of technological products. I specialize in Javascript technologies (MERN stack - MongoDB, Express JS, React JS, Node JS) to build complete web applications. I develop elegant solutions using open source tools and proactively look for ways to make systems run efficiently. In my free time as a cellist and composer, I write electronic pieces of music using modular synthesizers and digital instruments.</p>
             </div>
         </div>
