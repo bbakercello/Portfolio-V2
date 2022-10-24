8439e89e464565ad036382774d9c915b65b05305
bbakercello <bbakercello@gmail.com>
2022-10-24 15:12:35 -0400

    final changes made to responsive layout

--- .next/static/webpack/webpack.5e97f2e93da1fcb7.hot-update.js
+++ .next/static/webpack/webpack.5e97f2e93da1fcb7.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "b74411dccb1cac34"; }
+/******/ 	__webpack_require__.h = function() { return "e49abcf2ee13e7da"; }
 /******/ }();
 /******/ 
 /******/ }
