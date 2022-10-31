061f2e40dfb47e906c820b95a8924b9fd6f7cb04
bbakercello <bbakercello@gmail.com>
2022-10-31 11:19:16 -0400

    added customized information to learn more buttons and styled

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8f3b768a016d184d"; }
+/******/ 		__webpack_require__.h = function() { return "be50983bcfa15f4f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Work.js
+++ components/Work.js
@@ -11,19 +11,27 @@ const Work = (props) => {
     const work = [
         {name: 'Body Bell Records',
          href:'https://body-bell-frontend.vercel.app/',
-         description: 'Anonymous audio forum built', 
+         description: 'Indie Electronic Record Label', 
          background: 'https://i.imgur.com/cvn2bec.png',
-        tech: []},
-         {name: 'Anonimusic',
+        tech: 'NextJS, Tailwind, SpotifyAPI, MongoDB, Nodejs',
+        git: 'https://github.com/bbakercello/body-bell-frontend',
+        creators: 'Ben Baker'},
+         
+        {name: 'Anonimusic',
          href:'https://vocal-daffodil-44e825.netlify.app/',
-         description: 'Anonymous audio forum built',
-         tech: ['placeholder'],
-        background: 'https://i.imgur.com/mMkPoQ3.jpg'},
-         {name: 'LandPost',
+         description: 'Anonymous Audio Forum',
+         tech: 'MERN, Cloudinary, React-Audio-Player',
+        background: 'https://i.imgur.com/mMkPoQ3.jpg',
+        git: 'https://github.com/hspinks2692/anonimusic_frontend',
+        creators: 'Harvey Spinks / Tim Rathert / Ben Baker'},
+         
+        {name: 'LandPost',
          href:'https://landpost.herokuapp.com/blog',
-         description: 'Anonymous audio forum built',
-         tech: [],
-        background: 'https://i.imgur.com/NpRstnS.jpg'}
+         description: 'Environmental Social Media',
+         tech: 'Express, Nodejs, HTML, CSS',
+        background: 'https://i.imgur.com/NpRstnS.jpg',
+        git: 'https://github.com/bbakercello/Landpost',
+        creators: 'David Robles / Victoria Vela / Ben Baker'}
     ]
 
     return(
@@ -43,13 +51,15 @@ const Work = (props) => {
                     </a>
                     
                     </motion.div>
+                    
                     <Popover className="relative static flex justify-center">
       {({ open }) => (
-        <>
+        <><motion.div whileHover={{ scale: 1.2 }}>
           <Popover.Button className={`
                 ${open ? '' : 'text-opacity-90'}
-                group flex justify-center items-center rounded-md bg-sky-700 px-3 py-2 text-base font-medium text-white hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+                group flex justify-center items-center rounded-md bg-sky-700 px-3 h-8 text-base font-medium text-white hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
             ><span>Learn</span></Popover.Button>
+            </motion.div>
           <Popover.Overlay className="fixed inset-0 bg-black opacity-30" />
           <Transition
               as={Fragment}
@@ -57,13 +67,34 @@ const Work = (props) => {
               enterFrom="opacity-0 translate-y-1"
               enterTo="opacity-100 translate-y-0"
               leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
+              leaveFrom="opacity-100 translate-y-1"
               leaveTo="opacity-0 translate-y-1"
             >
-            <Popover.Panel className="absolute bottom-0 justify-center left-1/2 z-10 mt-3 w-60 max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
+            <Popover.Panel className="absolute bottom-0 justify-center left-1/2 z-10 w-64 max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
             
             <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-              <div className="relative grid gap-8 bg-white p-7 lg:grid-cols-2">
+              <div className="relative grid justify-center gap-1 bg-white p-3">
+                
+                <div >{work.description}</div>
+                <span className='text-sm'>Technology Used:</span>
+                <div className='text-xs'>{work.tech}</div>
+                <div className='text-sm'>Team:</div>
+                <div className='text-xs'>{work.creators}</div>
+                <div className='text-sky-700 flex justify-right'>
+                    <motion.div className='pr-2'whileHover={{ x: 100, scale: 1.2}}
+                                whileTap={{ scale: 0.8 }}
+                                style={{ x: 100 }}>
+                                    <a href={work.git}><FontAwesomeIcon icon={props.skills[9].icon}/>
+                                    </a>
+                    </motion.div>
+                    <motion.div className=''
+                                whileHover={{ x: 100, scale: 1.2}}
+                                whileTap={{ scale: 0.8 }}
+                                style={{ x: 100 }}>
+                                    <a href={work.href}>Sandbox
+                                    </a>
+                    </motion.div>
+                </div>
                 </div>
                 
                 </div>
