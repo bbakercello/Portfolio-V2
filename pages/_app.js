9ad58253ae0848c89782bbb321a142a6f3b9fb4d
bbakercello <bbakercello@gmail.com>
2023-01-05 13:42:28 -0500

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,43 @@
 import '../styles/base.css'
 import Document, { Head, Main, NextScript } from "next/document";
+import Script from 'next/script'
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag";
 
-
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {const router = useRouter();
+useEffect(() => {
+  const handleRouteChange = (url) => {
+    gtag.pageview(url);
+  };
+  router.events.on("routeChangeComplete", handleRouteChange);
+  return () => {
+    router.events.off("routeChangeComplete", handleRouteChange);
+  };
+}, [router.events]);
   return (
-
+    <>
+    
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-Z7XJH0317L"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-Z7XJH0317L', {
+            page_path: window.location.pathname,
+          });
+          `,
+        }}
+      />
       <Component {...pageProps} />
-
-  )
+    </>
+  );
 }
 
 export default MyApp
